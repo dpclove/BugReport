10c68743e1dc06ac216ea8ba3fcf05905c125a28
董鹏超 <dongpengchao@yingmob.com>
2019-07-18 14:57:18 +0800

    Update app.js

--- web-server/app.js
+++ web-server/app.js
@@ -4,35 +4,35 @@ var log4js = require('log4js');
 log4js.configure('./config/log4js.json');
 
 var logger = log4js.getLogger('index');
-// logger.info("11111111");
 
 // var indexRouter = require("./routes/index");
 
-// app.configure(function(){
-//   app.use(express.methodOverride());
-//   app.use(express.bodyParser());
-//   app.use(app.router);
-//   app.set('view engine', 'jade');
-//   app.set('views', __dirname + '/public');
-//   app.set('view options', {layout: false});
-//   app.set('basepath',__dirname + '/public');
-// });
-//
-// app.configure('development', function(){
-//   app.use(express.static(__dirname + '/public'));
-//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-// });
-//
-// app.configure('production', function(){
-//   var oneYear = 31557600000;
-//   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-//   app.use(express.errorHandler());
-// });
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(app.router);
+  app.set('view engine', 'jade');
+  app.set('views', __dirname + '/public');
+  app.set('view options', {layout: false});
+  app.set('basepath',__dirname + '/public');
+});
+
+app.configure('development', function(){
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.errorHandler());
+});
 
 app.post('/postReport', function (req, res) {
-  logger.info(req.body);
-  // logger.debug('hello world');
-  res.send('hello, express');
+  if(req.body) {
+    logger.info(req.body);
+  }
+  res.send({msg:'PostReport Success'});
 });
 // app.get('/postReport2', function (req, res) {
 //   res.send('hello, express');
