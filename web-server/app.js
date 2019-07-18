4cfa9c3d565828c2eeb3614f912af17d85b3c516
董鹏超 <dongpengchao@yingmob.com>
2019-07-18 14:12:29 +0800

    change
    change

--- web-server/app.js
+++ web-server/app.js
@@ -8,26 +8,26 @@ var logger = log4js.getLogger('index');
 
 // var indexRouter = require("./routes/index");
 
-app.configure(function(){
-  app.use(express.methodOverride());
-  app.use(express.bodyParser());
-  app.use(app.router);
-  app.set('view engine', 'jade');
-  app.set('views', __dirname + '/public');
-  app.set('view options', {layout: false});
-  app.set('basepath',__dirname + '/public');
-});
-
-app.configure('development', function(){
-  app.use(express.static(__dirname + '/public'));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-app.configure('production', function(){
-  var oneYear = 31557600000;
-  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
-  app.use(express.errorHandler());
-});
+// app.configure(function(){
+//   app.use(express.methodOverride());
+//   app.use(express.bodyParser());
+//   app.use(app.router);
+//   app.set('view engine', 'jade');
+//   app.set('views', __dirname + '/public');
+//   app.set('view options', {layout: false});
+//   app.set('basepath',__dirname + '/public');
+// });
+//
+// app.configure('development', function(){
+//   app.use(express.static(__dirname + '/public'));
+//   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+// });
+//
+// app.configure('production', function(){
+//   var oneYear = 31557600000;
+//   app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+//   app.use(express.errorHandler());
+// });
 
 app.post('/postReport', function (req, res) {
   logger.info(req.body);
